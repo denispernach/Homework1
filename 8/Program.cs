fb9295acf4ef64453fcdd4972f87bf713152e3df
denis <denispernach@yandex.ru>
2022-07-15 19:56:48 +0200

    написать четные числа

---
+++ 8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count<=N, )
+(N%2==0);
+    {Console.WriteLine(count);
+    count++;
+    }
+   
+   
+  
+
+    
+
+
+
