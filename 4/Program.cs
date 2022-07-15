55179e111009fdf231de996960c00b4014e955a4
denis <denispernach@yandex.ru>
2022-07-15 18:12:32 +0200

    максимум из трех чисел

---
+++ 4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число С: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int max=A;
+
+if (A> B && A > C)
+{
+max = A;
+Console.WriteLine ("max=A");
+}
+else if (B > A && B > C)
+{
+max = B;
+Console.WriteLine ("max=B");
+}
+else
+{
+max = C;
+Console.WriteLine ("max=C");
+}
+
+
+
+
