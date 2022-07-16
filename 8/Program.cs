02af6a06f2231a4dfc6993b997059df260b2bd3b
denis <denispernach@yandex.ru>
2022-07-16 12:38:28 +0200

    исправлено

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,11 +1,17 @@
-﻿Console.Write("Введите число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int count = 2;
-while (count<=N, )
-(N%2==0);
-    {Console.WriteLine(count);
-    count++;
-    }
+﻿int n = 2;
+
+Console.Write("Write number: ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+for (int i = n; i <= m; i++)
+        
+if (i % 2 == 0) 
+
+Console.Write(i);
+
+    
+
    
    
   
